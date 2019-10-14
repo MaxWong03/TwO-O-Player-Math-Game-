0b76cfc9144b153e9e52bbad3122f04e141cecde
MaxWong03 <madmaxmmajournal@gmail.com>
2019-10-14 18:25:53 +0000

    implement question class

--- main.rb
+++ main.rb
@@ -2,3 +2,7 @@ require 'pry'
 require './players'
 require './questions'
 require './games'
+
+
+player_1 = Player.new
+player_2 = Player.new
--- questions.rb
+++ questions.rb
@@ -1,2 +1,23 @@
+require 'pry'
+
 class Question
+  attr_accessor :question, :answer
+  def initialize (player)
+    @player = player
+  end
+
+  def ask_question
+    make_question
+    puts question
+    puts answer 
+  end
+
+  private
+  def make_question
+    num_1 = rand(1..20)
+    num_2 = rand(1..20)
+    self.question = "#{@player}: What is #{num_1} plus #{num_2} equal?"
+    self.answer = num_1 + num_2
+  end
 end
+
