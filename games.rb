0acb433f098a4afa58a3e2977b9d32761baecca2
MaxWong03 <madmaxmmajournal@gmail.com>
2019-10-14 18:35:10 +0000

    implemnet game_over? method

--- games.rb
+++ games.rb
@@ -1,2 +1,12 @@
 class Game
+  def initialize (player_1, player_2)
+    @players = [player_1, player_2]
+  end
+
+  def game_over?
+    loser = @players.select {|player| player.point == 0}
+    false unless loser.length > 0
+    true
+  end
 end
+
--- main.rb
+++ main.rb
@@ -6,3 +6,7 @@ require './games'
 
 player_1 = Player.new
 player_2 = Player.new
+
+game = Game.new(player_1, player_2)
+player_1.point = 0
+puts game.game_over?
